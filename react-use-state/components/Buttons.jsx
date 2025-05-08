8e7f5a638301012b49b68a10286c5121e8f2fc63
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-08 16:01:24 +0200

    [DONE] Ex DONE + 1 Bonus

--- react-use-state/components/Buttons.jsx
+++ react-use-state/components/Buttons.jsx
@@ -3,15 +3,29 @@ import { useState } from 'react'
 import languages from '../data/languages'
 
 const Buttons = () => {
-    return (
-        <div className='cont-buttons'>
-            {languages.map((language) => (
-                <button>
-                    <h2>{language.title}</h2>
-                </button>
 
-            ))}
-        </div>
+    const [openId, setOpenId] = useState(null)
+    const selectedLanguage = languages.find(language => language.id === openId)
+
+    return (
+        <>
+            {/* Bottoni */}
+            <div className='cont-buttons'>
+                {languages.map((language) => (
+                    <button key={language.id} onClick={() => setOpenId(language.id)}>
+                        <h3>{language.title}</h3>
+                    </button>
+                ))}
+            </div>
+
+            {/*card accordion*/}
+            {selectedLanguage && (
+                <div className='card'>
+                    <h2>{selectedLanguage.title}</h2>
+                    <p>{selectedLanguage.description}</p>
+                </div>
+            )}
+        </>
     )
 }
 
